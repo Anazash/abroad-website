9799716d9ec0b6956d5244064d09d4336dbe53cc
Anazash <anazash88@gmail.com>
2023-06-05 16:58:12 +0530

    study abroad template completed

--- abroadapp/urls.py
+++ abroadapp/urls.py
@@ -2,5 +2,18 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('canada',views.canada,name='canada'),
+    path('franchise',views.franchise,name='franchise'),
+    path('award',views.award,name='award'),
+    path('presencemedia',views.presencemedia,name='presencemedia'),
+    path('photogallery',views.photogallery,name='photogallery'),
+    path('blog',views.blog,name='blog'),
+    path('branches',views.branches,name='branches'),
+    path('ielts',views.ielts,name='ielts'),
+    path('migratecanada',views.migratecanada,name='migratecanada'),
+    path('readmore',views.readmore,name='readmore'),
+    path('seminar',views.seminar,name='seminar'),
+    path('signup',views.signup,name='signup'),
+    path('register',views.register,name='register'),
 
 ]
--- abroadapp/views.py
+++ abroadapp/views.py
@@ -4,3 +4,42 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def canada(request):
+    return render(request,'canada.html')
+
+def franchise(request):
+    return render(request,'franchise.html')
+
+def award(request):
+    return render(request,'award.html')
+
+def presencemedia(request):
+    return render(request,'presencemedia.html')
+
+def photogallery(request):
+    return render(request,'photogallery.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def branches(request):
+    return render(request,'branches.html')
+
+def ielts(request):
+    return render(request,'ielts.html')
+
+def migratecanada(request):
+    return render(request,'migratecanada.html')
+
+def readmore(request):
+    return render(request,'readmore.html')
+
+def seminar(request):
+    return render(request,'seminar.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def register(request):
+    return render(request,'register.html')
